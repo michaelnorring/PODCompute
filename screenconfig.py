9bfa5ed4c9af7d40d7a10832ef43ac723fc87927
Jan-Michael Norring <MichaelNorring@MTRAINIER-MacBook-Pro-2.local>
2018-05-19 08:56:48 -0700

    Updated ifconfig

--- screenconfig.py
+++ screenconfig.py
@@ -13,43 +13,28 @@ class ScreenConfig(Screen):
         currentOS = ""
         if platform == "darwin": 
             currentOS = "Mac OS X (Darwin)"
-            self.ifconfig_Darwin()
+            self.ifconfig('en0')
         elif platform == "win32":
             currentOS = "Windows"
-            self.ifconfig_Win32()
+            self.ifconfig('none')
         elif platform == "linux" or platform == "linux2":
             currentOS = "Linux"
-            self.ifconfig_Linux()
+            self.ifconfig('wlan0')
         else:
             currentOS = platform
 
         currentOSLabel.text = currentOS
 
-    def ifconfig_Darwin(self):
-        ifconfig = check_output(['ifconfig', 'en0'])
-        iface, status, MAC, Bcast, Nmask, IPv6 = (ifconfig.split()[i] for i in (0,24, 5, 18, 16, 14))
-
-        ifacelabel = self.ids['IFace']
-        ifacelabel.text = iface
+    def ifconfig(self, interface):
+        ifacelabel = self.ids['Interface']
+        ifacelabel.text = interface
 
         statuslabel = self.ids['Status']
-        statuslabel.text = status
 
-        iplabel = self.ids['IPAddress']
-        iplabel.text = IPv6
-
-        maclabel = self.ids['MACAddress']
-        maclabel.text = MAC
-
-        bcastlabel = self.ids['BCast']
-        bcastlabel.text = Bcast
-
-        nmarklabel = self.ids['NMask']
-        nmarklabel.text = Nmask
-
-    def ifconfig_Win32(self):
-        pass
-
-    def ifconfig_Linux(self):
-        pass
+        if interface != 'none':
+            ifconfig = check_output(['ifconfig', interface])
+            
+            statuslabel.text = ifconfig
+        else:
+            statuslabe.text = '' 
     
